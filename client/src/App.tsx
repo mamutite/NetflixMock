2d9fdce3a5c2656d4a5cac9a2f13499044919f19
Stefan Galabov <sgalabov22@gmail.com>
2022-07-08 15:45:13 +0300

    Project TS setup and first Browse component

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { BrowseContainer } from "./components";
+
+function App() {
+  return (
+    <div className="App">
+      <BrowseContainer />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/browse-page/BrowseContainer.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export function BrowseContainer() {
+    return (
+        <h1>Alo da</h1>
+    );
+}
